a47615c4bd01990663d70e70eb9af0cd8447ad4c
Gabriel Pasparan <pasparangabriel@yahoo.com>
2019-12-23 19:35:15 +0000

    Add ui Button

---
+++ src/ui/Button/Button.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import './Button.css'
+
+const Button = (props) => {
+  let className = 'btn', onClick
+
+  className += props.type ? ' ' + props.type : ''
+  className += props.position ? ' ' + props.position : ''
+  className += props.size ? ' ' + props.size : ''
+  onClick = props.onClick
+
+  return (
+    <button 
+      className={className}
+      onClick={onClick}
+    >
+      {props.children}
+    </button>
+  )
+}
+
+export { Button }
