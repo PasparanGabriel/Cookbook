f8fcdaa3d30b636513ea6b6769750f8debb159bb
Gabriel Pasparan <pasparangabriel@yahoo.com>
2019-12-28 19:03:57 +0000

    Updates

---
+++ precache-manifest.f2acdbe8cbc4eef101e4c7ab411c0a68.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d4449981f4657ed79e76d01c7ec0742",
+    "url": "/cookbook/index.html"
+  },
+  {
+    "revision": "ef06acef4470139fa9f5",
+    "url": "/cookbook/static/css/main.562cd844.chunk.css"
+  },
+  {
+    "revision": "ee1448e7a8884a0aa142",
+    "url": "/cookbook/static/js/2.a8a5fd2a.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/cookbook/static/js/2.a8a5fd2a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ef06acef4470139fa9f5",
+    "url": "/cookbook/static/js/main.84422da7.chunk.js"
+  },
+  {
+    "revision": "700072f4d372f2c9fc83",
+    "url": "/cookbook/static/js/runtime-main.3b257241.js"
+  }
+]);
