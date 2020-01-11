134be0542086de2d10d7a3c1676f090f65036644
PasparanGabriel <pasparangabriel@yahoo.com>
2020-01-11 15:49:18 +0000

    Add ui for  Loading

---
+++ src/ui/Loading/Loading.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Loading.css'
+
+export const Loading = () => (
+  <div className='loading'>
+    <span>Loading</span>
+    <span className='bounce ball1' />
+    <span className='bounce ball2' />
+    <span className='bounce ball3' />
+  </div>
+)
