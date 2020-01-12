71dd01aad4be5bed5ddb117998f0ef070f01c656
PasparanGabriel <pasparangabriel@yahoo.com>
2020-01-12 17:47:50 +0000

    Updates

---
+++ precache-manifest.6964e1b9a65c94a0916d61d5aeb942d2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70dc624875007d33cac4e782b8ac0ac8",
+    "url": "/cookbook/index.html"
+  },
+  {
+    "revision": "fa06aab0496066020319",
+    "url": "/cookbook/static/css/main.74963525.chunk.css"
+  },
+  {
+    "revision": "66160edf48267ff6fcc3",
+    "url": "/cookbook/static/js/2.0c64ea0c.chunk.js"
+  },
+  {
+    "revision": "3160bf7c029258aba92ee24744af9381",
+    "url": "/cookbook/static/js/2.0c64ea0c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fa06aab0496066020319",
+    "url": "/cookbook/static/js/main.30ccad2d.chunk.js"
+  },
+  {
+    "revision": "700072f4d372f2c9fc83",
+    "url": "/cookbook/static/js/runtime-main.3b257241.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/cookbook/precache-manifest.0c3e764f156e2e8d8a3f8d17d2910ba3.js"
+  "/cookbook/precache-manifest.6964e1b9a65c94a0916d61d5aeb942d2.js"
 );
 
 self.addEventListener('message', (event) => {
